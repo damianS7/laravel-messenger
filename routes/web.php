8475ee3749ed479842b4e371ecd90e9d83f4c367
damianS7 <josepwnz@gmail.com>
2019-11-28 20:17:34 +0100

    rutas actualizadas

--- routes/web.php
+++ routes/web.php
@@ -22,30 +22,17 @@ Route::get('/people', 'PeopleController@index');
 // Contactos disponibles
 Route::get('/contacts', 'ContactController@index');
 
-// Datos de perfil
-Route::get('/profile', 'ProfileController@index');
-
-
-
-// Conversacion id y sus mensajes
-//Route::get('/conversation/{contact_id}', 'ConversationController@index');
-
-
-
-
-// Almacenar nuevo mensaje de conversacion
-Route::post('/conversation', 'ConversationController@storeMessage');
-
-// ======
-
-// Ruta para peticion incial de la app. Devuelve toda la info necesaria
-Route::get('/fetchAll', 'MessengerController@index');
-
-// Ruta para recuperar los nuevos mensajes recibidos.
-Route::get('/conversations/update', 'ConversationController@fetchLastMessages');
-
 // Ruta para actualizar el perfil
 Route::put('/profile/{profile_id}', 'ProfileController@update');
 
+// Datos de usuario y su perfil
+Route::get('/profile', 'ProfileController@index');
+
 // Ruta para postear nuevos mensajes
 Route::post('/conversation/{conversation_id}', 'ConversationController@storeMessage');
+
+// Conversaciones de usuario
+Route::get('/conversations', 'ConversationController@index');
+
+// Ruta para recuperar los nuevos mensajes recibidos.
+Route::get('/conversations/update', 'ConversationController@fetchLastMessages');
