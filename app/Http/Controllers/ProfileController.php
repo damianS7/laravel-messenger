8e34afe9c8ea8866ba76dc92f86e9ee7c264950c
damianS7 <josepwnz@gmail.com>
2019-11-29 18:23:03 +0100

    Limpieza de codigo no usado

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,16 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Profile;
 use Auth;
-use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
     public function __construct()
     {
         // Se necesita esta autentificado para llevar a cabo acciones
-        // de CRUD sobre el perfil
         $this->middleware('auth');
     }
 
@@ -43,44 +42,12 @@ class ProfileController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
     {
         //
     }
