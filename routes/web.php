826bed956e0bee9c70320b61701cf3e3158c93b4
damianS7 <josepwnz@gmail.com>
2019-12-09 23:36:39 +0100

    Eliminadas rutas sin uso

--- routes/web.php
+++ routes/web.php
@@ -15,27 +15,17 @@ Route::get('/', 'HomeController@index');
 
 Auth::routes();
 
-// Gente disponible en el chat
-Route::get('/people', 'PeopleController@index');
-
-// Contactos disponibles
-Route::get('/contacts', 'ContactController@index');
 // Elimina un contacto
 Route::delete('/contacts/{contact_id}', 'ContactController@destroy');
+
 // Agrega un contacto
 Route::post('/contacts', 'ContactController@store');
 
 // Ruta para actualizar el perfil
 Route::put('/profile/{profile_id}', 'ProfileController@update');
 
-// Datos de usuario y su perfil
-Route::get('/profile', 'ProfileController@index');
-
 // Ruta para postear nuevos mensajes
 Route::post('/conversation/{conversation_id}', 'MessageController@store');
-
-// Conversaciones de usuario
-Route::get('/conversations', 'ConversationController@index');
 
 // Ruta para recuperar los nuevos mensajes recibidos.
 Route::get('/conversations/update', 'MessageQueueController@index');
