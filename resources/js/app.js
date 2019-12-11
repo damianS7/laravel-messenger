5fbfdb720aa2b54246b926e0ba893a265c5c678d
damianS7 <josepwnz@gmail.com>
2019-12-12 00:41:26 +0100

    Eliminado mensaje de advertencia

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,7 +16,7 @@ require('./bootstrap');
 
 
 window.Vue = require('vue');
-
+Vue.config.productionTip = false
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
